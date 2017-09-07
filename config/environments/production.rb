f7f2ed5fb02bfa290eca5be594d173cbb08b3f70
pennygibson <cenneboulet@yahoo.ca>
2017-09-07 20:41:04 +0000

    Hopefully fixed all the things.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,10 +86,3 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-Traitify.configure do |traitify|
-  traitify.host = "https://api-sandbox.traitify.com"
-  traitify.version = "v1"
-  traitify.secret_key = ENV["SECRET_KEY"]
-  traitify.public_key =  ENV["PUBLIC_KEY"] # Optional
-  traitify.locale_key = "en-us"  # Optional
-end
---
+++ config/initializers/traitify.rb
@@ -0,0 +1,8 @@
+# require 'traitify'
+Traitify.configure do |traitify|
+   traitify.host = "https://api-sandbox.traitify.com"
+   traitify.version = "v1"
+   traitify.secret_key = ENV["SECRET_KEY"]
+   traitify.public_key =  ENV["PUBLIC_KEY"] # Optional
+   traitify.deck_id = "core"
+ end
