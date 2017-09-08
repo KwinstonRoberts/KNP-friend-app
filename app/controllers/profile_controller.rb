d47947f5609ddaa7837010868b8b0eedf933b71b
Nam Yoo <namgooy@gmail.com>
2017-09-08 20:14:34 +0000

    Added Profile controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,5 +4,9 @@ class ProfileController < ApplicationController
   #   https://api.traitify.com/v1/assessments/{assessment_id}?data=blend,types,traits,career_matches
 
   # end
+  def index
+    # Look up user by session ID, get their id => personality (asessment ID), and call it
+    # @user_find_by_id
+  end
 
 end
