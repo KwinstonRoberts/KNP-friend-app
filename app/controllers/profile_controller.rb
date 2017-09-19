add8f4b5421e017c9fe5c6cf931a7c856bb41fc9
KwinstonRoberts <kyle.w.r@hotmail.com>
2017-09-19 22:02:40 +0000

    fixing matches

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,41 +6,11 @@ class ProfileController < ApplicationController
       @traitify = Traitify.new
       @assessment = @traitify.find_results(@user.result.assessment_id)
       @traits = @user.result.traits.first
-      @matches = get_matches
+      @matches = User.all()
       @activities = Activity.all()
       @user = User.find(session[:user_id])
     else
       redirect_to test_index_path
     end
-  end
-  def get_deck(res)
-    res.result.personalities.first.name
-  end
-  def get_ptype(res)
-    res.result.personalities.first.name
-  end
-  def compare_ptype(res)
-    if get_ptype(res) ===  get_ptype(@user)
-      return true
-    else
-      res.result.personalities.each do |r|
-        if(r.name === get_ptype(@user))
-          avg = (r.score + @user.result.personalities.first.score)/2
-          if avg >= 65
-            puts avg
-            return true
-          else
-            return false
-          end
-        end
-      end
-    end
-  end
-  def get_matches
-    matches = []
-    User.all().each do |match|
-      matches.push(match)
-    end
-    return matches
   end
 end
