4fd1dee662dfbe327858cda8a6624eacabc6176e
KwinstonRoberts <kyle.w.r@hotmail.com>
2017-09-20 00:47:59 +0000

    changing match style

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -13,30 +13,8 @@ class ProfileController < ApplicationController
       redirect_to test_index_path
     end
   end
-  def get_deck(res)
-    res.result.personalities.first.name
-  end
-  def get_ptype(res)
-    res.result.personalities.first.name
-  end
-  def compare_ptype(res)
-    if get_ptype(res) ===  get_ptype(@user)
-      return true
-    else
-      res.result.personalities.each do |r|
-        if(r.name === get_ptype(@user))
-          avg = (r.score + @user.result.personalities.first.score)/2
-          if avg >= 75
-            puts avg
-            return true
-          else
-            return false
-          end
-        end
-      end
-    end
-  end
+  
   def get_matches
-    return User.includes(result: [:personalities,:traits]).where('traits.name=? or personalities.name=?',@traits.name, @user.result.personalities.first.name).references(:personalities,:traits)
+    return User.includes(result: [:personalities,:traits]).where('traits.name=? or personality=?',@traits.name, @user.result.personalities.first.name).references(:personalities,:traits)
   end
 end
