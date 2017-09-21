49b63f9bc254f1d74667937a8c47a16ec608bc67
KwinstonRoberts <kyle.w.r@hotmail.com>
2017-09-21 16:22:02 +0000

    sanitized results

--- app/jobs/channel_job.rb
+++ app/jobs/channel_job.rb
@@ -2,8 +2,10 @@ class ChannelJob < ApplicationJob
   queue_as :default
 
   def perform(ptype,user)
+
+tags = %w(a acronym b strong i em li ul ol h1 h2 h3 h4 h5 h6 blockquote br cite sub sup ins p)
     ActionCable.server.broadcast user,{
-      messages: sanitize(Message.where(channel:ptype)),
+      messages: sanitize(Message.where(channel:ptype),tags: tags, attributes: %w(href title)),
     }
   end
 end
--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -3,9 +3,9 @@ class MessageBroadcastJob < ApplicationJob
 
   def perform(message)
     ActionCable.server.broadcast 'chat_channel_' + message.channel,
-      message: sanitize(message.body),
-      user: sanitize(message.user_id),
+      message: sanitize(message.body,tags: tags, attributes: %w(href title)),
+      user: sanitize(message.user_id,tags: tags, attributes: %w(href title)),
       ptype: User.find(message.user_id).result.image,
-      name:  sanitize(User.find(message.user_id).name)
+      name:  sanitize(User.find(message.user_id).name,tags: tags, attributes: %w(href title))
   end
 end
